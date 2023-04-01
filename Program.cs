4ed8a981200b1aa46307a3d5ebe75c0d61ddc00a
Bléqui <gustavopregigueiro6@gmail.com>
2023-04-01 15:03:04 -0300

    Adição do código

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+String Frase, FraseDory;
+
+Console.WriteLine("Digite uma frase para a Dory dizer em baleiês");
+Frase = Console.ReadLine()!;
+
+FraseDory = Frase
+
+.Replace("A", "AAA")
+.Replace("a", "aaa")
+.Replace("E", "EEE")
+.Replace("e", "eee")
+.Replace("I", "III")
+.Replace("i", "iii")
+.Replace("O", "OOO")
+.Replace("o", "ooo")
+.Replace("U", "UUU")
+.Replace("u", "uuu")
+.Replace("Á", "ÁÁÁ")
+.Replace("á", "ááá")
+.Replace("Ã", "ÃÃÃ")
+.Replace("ã", "ããã")
+.Replace("Õ", "ÕÕÕ")
+.Replace("õ", "õõõ");
+
+Console.WriteLine($"Dory: {FraseDory}");
+
+
+
